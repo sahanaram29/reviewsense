ed0969ad9daee67110213ffa8e7aec50fb9a95ea
sahanaram29 <125304639+sahanaram29@users.noreply.github.com>
2023-08-15 11:45:34 -0500

    Updating tokenize function and comments

--- module2.py
+++ module2.py
@@ -10,7 +10,6 @@ def sentiment():
     df = t1.rename_columns()
     sentiments = SentimentIntensityAnalyzer()
     # Create sentiment column
-    # Calculate sentiment scores for each review and assign 'positive' or 'negative' based on the scores
     sentiment_labels = []
     for review_text in df['reviews_text']:
         sentiment_scores = sentiments.polarity_scores(review_text)
@@ -24,17 +23,13 @@ def sentiment():
 
 def process_text():
     df = sentiment()
-    # Tokenize the reviews_text column using nltk's word_tokenize function
     df['reviews_text'] = df['reviews_text'].apply(lambda text: word_tokenize(text))
-    # Normalize the tokenized words using the normalize function from settings
     df['reviews_text'] = df['reviews_text'].apply(normalize)
 
     return df
 
 def export_the_dataset():
-    # Call process_text() to get the cleaned dataset with sentiment analysis and tokenization
     df = process_text()
-    # Export the cleaned dataset to a new CSV file named 'ecommerce.csv'. use index = False.
     export_path = 'ecommerce.csv'
     df.to_csv(export_path, index=False)
     return df
